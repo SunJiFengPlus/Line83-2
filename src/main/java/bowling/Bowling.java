2eb8b7422f7be5d230b8c8d6833db476113f738a
孙继峰 <sun.jifeng@outlook.com>
2020-10-27 20:28:45 +0800

    format

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -3,7 +3,7 @@ package bowling;
 // Please don't modify the class name.
 public class Bowling {
     int score;
-    int rolls[] = new int[21];
+    int[] rolls = new int[21];
     int rollsIndex = 0;
 
     // Please don't modify the signature of this method.
@@ -15,39 +15,39 @@ public class Bowling {
     // Please don't modify the signature of this method.
     public int getScore() {
         int score = 0;
-		int frameIndex = 0;
-		for(int frame = 0; frame < 10; frame++ ) {
-			if ( isStrike(frameIndex) ) {
-				score += strikeBonus(frameIndex);
-				frameIndex += 1;
-			} else if ( isSpare(frameIndex) ) {
-				score += spareBonus(frameIndex);
-				frameIndex += 2;
-			} else {
-				score += sumOfBallsInFrame(frameIndex);
-				frameIndex += 2;
-			}
-		}
-		return score;
+        int frameIndex = 0;
+        for (int frame = 0, finalFrame = 10; frame < finalFrame; frame++) {
+            if (isStrike(frameIndex)) {
+                score += strikeBonus(frameIndex);
+                frameIndex += 1;
+            } else if (isSpare(frameIndex)) {
+                score += spareBonus(frameIndex);
+                frameIndex += 2;
+            } else {
+                score += sumOfBallsInFrame(frameIndex);
+                frameIndex += 2;
+            }
+        }
+        return score;
     }
 
-	private int sumOfBallsInFrame(int frameIndex) {
-		return rolls[frameIndex] + rolls[frameIndex+1];
-	}
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
 
-	private int spareBonus(int frameIndex) {
-		return 10 + rolls[frameIndex+2];
-	}
+    private int spareBonus(int frameIndex) {
+        return 10 + rolls[frameIndex + 2];
+    }
 
-	private int strikeBonus(int frameIndex) {
-		return 10 + rolls[frameIndex+1] + rolls[frameIndex+2];
-	}
+    private int strikeBonus(int frameIndex) {
+        return 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    }
 
-	private boolean isStrike(int frameIndex) {
-		return rolls[frameIndex] == 10;
-	}
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
+    }
 
-	private boolean isSpare(int frameIndex) {
-		return rolls[frameIndex] + rolls[frameIndex+1] == 10;
-	}
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+    }
 }
