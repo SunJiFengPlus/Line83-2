ec3181dd350daecddc1703e5f470dceb258c98a4
孙继峰 <sun.jifeng@outlook.com>
2020-10-28 09:58:54 +0800

    一步替换, 旧的再见

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -2,7 +2,6 @@ package bowling;
 
 // Please don't modify the class name.
 public class Bowling {
-    int score;
     int[] rolls = new int[21];
     int rollsIndex = 0;
 
@@ -16,38 +15,11 @@ public class Bowling {
     public int getScore() {
         int score = 0;
         int frameIndex = 0;
-        for (int frame = 0, finalFrame = 10; frame < finalFrame; frame++) {
-            if (isStrike(frameIndex)) {
-                score += strikeBonus(frameIndex);
-                frameIndex += 1;
-            } else if (isSpare(frameIndex)) {
-                score += spareBonus(frameIndex);
-                frameIndex += 2;
-            } else {
-                score += sumOfBallsInFrame(frameIndex);
-                frameIndex += 2;
-            }
+        for (int round = 0, finalRound = 10; round < finalRound; round++) {
+            AbsFrame frame = AbsFrame.of(rolls, frameIndex);
+            score += frame.getScore();
+            frameIndex += frame.getIndexGap();
         }
         return score;
-    }
-
-    private int sumOfBallsInFrame(int frameIndex) {
-        return rolls[frameIndex] + rolls[frameIndex + 1];
-    }
-
-    private int spareBonus(int frameIndex) {
-        return 10 + rolls[frameIndex + 2];
-    }
-
-    private int strikeBonus(int frameIndex) {
-        return 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
-    }
-
-    private boolean isStrike(int frameIndex) {
-        return rolls[frameIndex] == 10;
-    }
-
-    private boolean isSpare(int frameIndex) {
-        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
     }
 }
